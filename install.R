3e3dff7aaafad3932effaaef04a8e5be04e3e559
Ben Marwick <benmarwick@gmail.com>
2019-01-11 00:48:26 -0800

    add screenshots of shiny, install.R for Binder, more text

---
+++ install.R
@@ -0,0 +1,12 @@
+# This is for binder, to install pkgs before we start
+install.packages(c("ggplot2",
+                   "cowplot",
+                   "knitr",
+                   "rmarkdown",
+                   "devtools",
+                   "dplyr",
+                   "bookdown",
+                   "xtable",
+                   "here",
+                   "rticles",
+                   "testthat"))
