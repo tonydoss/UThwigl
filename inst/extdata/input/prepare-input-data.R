c058b35bc6df452ec8f0cb4af5e4857ef5f9f805
Anthony Dosseto <tonyd@uow.edu.au>
2019-01-24 21:54:39 +1100

    added sample data for csUTh (Pan2018.csv from https://doi.org/10.1016/j.margeo.2018.01.012)

--- inst/extdata/input/prepare-input-data.R
+++ inst/extdata/input/prepare-input-data.R
@@ -3,7 +3,7 @@
 # assuming the working directory is the directory containing this file
 Hobbit_1_1T_for_iDAD <- read.csv("Hobbit_1-1T_for_iDAD.csv")
 Hobbit_MH2T_for_iDAD <- read.csv("Hobbit_MH2T_for_iDAD.csv")
-iolite_export <- read.csv("example_data_csUTh.csv")
+iolite_export <- read.csv("Pan2018.csv")
 
 
 # create pkg data objects for easy re-use
