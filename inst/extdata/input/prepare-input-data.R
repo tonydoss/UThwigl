1f3fec5845a1cec6f17cd5a5f411ca096acc511d
Ben Marwick <benmarwick@hotmail.com>
2019-01-24 22:13:56 -0800

    Pan2018 data should be called that

--- inst/extdata/input/prepare-input-data.R
+++ inst/extdata/input/prepare-input-data.R
@@ -3,10 +3,10 @@
 # assuming the working directory is the directory containing this file
 Hobbit_1_1T_for_iDAD <- read.csv("Hobbit_1-1T_for_iDAD.csv")
 Hobbit_MH2T_for_iDAD <- read.csv("Hobbit_MH2T_for_iDAD.csv")
-iolite_export <- read.csv("Pan2018.csv")
+Pan2018 <- read.csv("Pan2018.csv")
 
 
 # create pkg data objects for easy re-use
 usethis::use_data(Hobbit_1_1T_for_iDAD, Hobbit_1_1T_for_iDAD, overwrite = TRUE)
 usethis::use_data(Hobbit_MH2T_for_iDAD, Hobbit_MH2T_for_iDAD, overwrite = TRUE)
-usethis::use_data(iolite_export, iolite_export, overwrite = TRUE)
+usethis::use_data(Pan2018, Pan2018, overwrite = TRUE)
